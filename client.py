e46f5ff5acaeab7fb779b2f6fa6a45e997d07bef
micies <90556500+micies@users.noreply.github.com>
2021-11-22 11:39:52 +0200

    Update client.py

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ import socket
 
 
 
-# Initialise socket stuff
-TCP_IP = "127.0.0.1" # Only a local server
-TCP_PORT = 12345 # Just a random choice
-BUFFER_SIZE = 1024 # Standard chioce
+
+TCP_IP = (input('send to ip')) #"127.0.0.1"
+TCP_PORT = 12345 
+BUFFER_SIZE = 1024 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 file_name = input('send the link!...')
 def conn():
